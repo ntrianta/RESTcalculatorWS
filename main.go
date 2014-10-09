50318017e0b165fbe973026dc0273e1fc93fb76e
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2014-10-09 14:04:53 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+/* This is a simple calculator exposed as a web service.     *
+** Part of the assignment for Essential Skills module Lab 10.*
+** University of Amsterdam                                   *
+** MSc In System and Network Engineering 					 *
+** Nick Triantafyllidis										 *
+** This will be a RESTful service							 *
+*/
+
+package main
+
+import(
+	//net/http
+	fmt
+)
+
+func main(){
+	fmt.Println("Hello! This is gonna REST!")
+}
